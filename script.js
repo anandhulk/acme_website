4143eb4805c1d82c47cfd199b9056883dadeaa58
anandhulk <anandhulal7@gmail.com>
2023-06-15 10:02:12 +0530

    added cart page inside the product-description page, designed shop page's products container using grid layout

---
+++ script.js
@@ -0,0 +1,14 @@
+const shoppingCart=document.getElementById("shoppingCart")
+const overlay=document.getElementById("overlay")
+const addToCart=document.getElementById("addToCartButton")
+const cartExit=document.getElementById("cartExit")
+
+addToCart.addEventListener("click",()=>{
+    shoppingCart.classList.remove('hidden')
+    overlay.classList.remove('hidden')
+})
+
+cartExit.addEventListener("click",()=>{
+    shoppingCart.classList.add('hidden')
+    overlay.classList.add('hidden')
+})
